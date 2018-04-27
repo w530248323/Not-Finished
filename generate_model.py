4110fe4b3a18ec0de3a151c556de1d84e93cd6df
Wang Jingyao <530248323@qq.com>
2018-04-27 11:50:59 +0800

    Update generate_model.py

--- generate_model.py
+++ generate_model.py
@@ -1,12 +1,12 @@
 import torch
 from torch import nn
-from models import resnet, pre_act_resnet, wide_resnet, resnext, densenet
+from models import resnet, pre_act_resnet, wide_resnet, resnext, densenet, pseudonet
 
 
 def generate_model(opt):
 
     global model
-    assert opt.model in ['resnet', 'preresnet', 'wideresnet', 'resnext', 'densenet']
+    assert opt.model in ['resnet', 'preresnet', 'wideresnet', 'resnext', 'densenet', 'pseudonet']
 
     if opt.model == 'resnet':
         assert opt.model_depth in [10, 18, 34, 50, 101, 152, 200]
@@ -15,43 +15,50 @@ def generate_model(opt):
             model = resnet.resnet10(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 18:
             model = resnet.resnet18(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 34:
             model = resnet.resnet34(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 50:
             model = resnet.resnet50(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 101:
             model = resnet.resnet101(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 152:
             model = resnet.resnet152(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 200:
             model = resnet.resnet200(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
 
     elif opt.model == 'wideresnet':
@@ -62,7 +69,8 @@ def generate_model(opt):
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
                 k=opt.wide_resnet_k,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
 
     elif opt.model == 'resnext':
@@ -73,21 +81,24 @@ def generate_model(opt):
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
                 cardinality=opt.resnext_cardinality,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 101:
             model = resnext.resnet101(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
                 cardinality=opt.resnext_cardinality,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 152:
             model = resnext.resnet152(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
                 cardinality=opt.resnext_cardinality,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
 
     elif opt.model == 'preresnet':
@@ -97,37 +108,43 @@ def generate_model(opt):
             model = pre_act_resnet.resnet18(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 34:
             model = pre_act_resnet.resnet34(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 50:
             model = pre_act_resnet.resnet50(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 101:
             model = pre_act_resnet.resnet101(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 152:
             model = pre_act_resnet.resnet152(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 200:
             model = pre_act_resnet.resnet200(
                 num_classes=opt.n_classes,
                 shortcut_type=opt.resnet_shortcut,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
 
     elif opt.model == 'densenet':
@@ -135,27 +152,64 @@ def generate_model(opt):
         if opt.model_depth == 121:
             model = densenet.densenet121(
                 num_classes=opt.n_classes,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 169:
             model = densenet.densenet169(
                 num_classes=opt.n_classes,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 201:
             model = densenet.densenet201(
                 num_classes=opt.n_classes,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
         elif opt.model_depth == 264:
             model = densenet.densenet264(
                 num_classes=opt.n_classes,
-                sample_size=opt.sample_size,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
                 sample_duration=opt.sample_duration)
 
-    gpus = [int(i) for i in opt.gpus.split(',')]
-    print("=> active GPUs: {}".format(opt.gpus))
-    model = torch.nn.DataParallel(model, device_ids=gpus).cuda()
+    elif opt.model == 'pseudonet':
+        assert opt.model_depth in [15, 31, 63, 131, 199]
+        if opt.model_depth == 15:
+            model = pseudonet.P3D63(
+                num_classes=opt.n_classes,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
+                sample_duration=opt.sample_duration)
+        if opt.model_depth == 31:
+            model = pseudonet.P3D63(
+                num_classes=opt.n_classes,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
+                sample_duration=opt.sample_duration)
+        if opt.model_depth == 63:
+            model = pseudonet.P3D63(
+                num_classes=opt.n_classes,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
+                sample_duration=opt.sample_duration)
+        elif opt.model_depth == 131:
+            model = pseudonet.P3D131(
+                num_classes=opt.n_classes,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
+                sample_duration=opt.sample_duration)
+        elif opt.model_depth == 199:
+            model = pseudonet.P3D199(
+                num_classes=opt.n_classes,
+                sample_height=opt.sample_height,
+                sample_width=opt.sample_width,
+                sample_duration=opt.sample_duration)
+
+    # gpus = [int(i) for i in opt.gpus.split(',')]
+    # model = torch.nn.DataParallel(model, device_ids=gpus).cuda()
+    model = model.cuda()
 
     if opt.pretrain_path:
         print('loading pretrained model {}'.format(opt.pretrain_path))
@@ -177,4 +231,3 @@ def generate_model(opt):
         return model, parameters
 
     return model
-
